f961951ad4f84e08e1b59e76f92e3f5ad7360aad
Marisa <marisa.geyer@gmail.com>
2021-06-28 20:44:25 +0200

    tools for tutorial

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,89 +1,82 @@
 import time
+import os
+from base64 import b64encode
+from IPython.core.display import display, HTML
 
+def time_per_bin(archive):
+    time = archive.get_Integration(0).get_folding_period()
+    time_per_bin = time/archive.get_nbin()
+    return time_per_bin
 
-
-#def hangman():
-#    name = input("What is your name? ")
-#    print("Hello, " + name, "Time to play hangman!")
-#    time.sleep(1)
-#    print("What do you think is causing the pulsar light to dim?")
-#    time.sleep(1)
-#    print("Start guessing...")
-#    time.sleep(0.5)
-#    
-#    word = "eclipse"
-#    guesses = ''
-#    turns = 10
-#    
-#    #check if the turns are more than zero
-#    while turns > 0: 
-#        print("\n")
-#    
-#        failed = 0             
-#        for char in word:
-#            if char in guesses:     
-#                print(char,end=' ')    
-#            else:
-#                print("_",end=' ')    
-#                # increase the failed counter with one
-#                failed += 1    
-#    
-#        if failed == 0:        
-#            print ("\n\That's right! \nNow have a look at http://www.jb.man.ac.uk/~bretonr/doublepulsar/movies/doublepulsar_eclipse.mov to learn more.")
-#            break              
-#        guess = input("\n\nguess a character:") 
-#        guesses += guess                    
-#    
-#        if guess not in word:  
-#         # turns counter decreases
-#            turns -= 1        
-#            print ("\nWrong")    
-#            # how many turns are left
-#            print("\nYou have", + turns, 'more guesses')
-#            if turns == 0:           
-#                print("\nOops! You lose! But you can try again!")
-
-
-
-def hangman_py2():
-    name = raw_input("What is your name? ")
-    print "Hello, " + name, "Time to play hangman!"
+def hangman():
+    name = input("What is your name? ")
+    print("\n")
+    print("\nHello, " + name, "time to play hangman!")
+    time.sleep(2)
+    print("\n")
+    print("\nWhat do you think is causing the pulsar light to dim?")
     time.sleep(1)
-    print "What do you think is causing the pulsar light to dim?"
-    time.sleep(1)
-    print "Start guessing..."
+    print("\n")
+    print("Start guessing...")
     time.sleep(0.5)
-   
+    
     word = "eclipse"
     guesses = ''
     turns = 10
-   
+    
     #check if the turns are more than zero
-    while turns > 0:         
+    while turns > 0: 
+        print("\n")
+    
         failed = 0             
-        for char in word:      
-            if char in guesses:    
-                print char,    
+        for char in word:
+            if char in guesses:     
+                print(char,end=' ')    
             else:
-                print "_",     
-                #increase the failed counter with one
+                print("_",end=' ')    
+                # increase the failed counter with one
                 failed += 1    
-   
-        if failed == 0:        
-            print "\nThat's right! \nNow have a look at http://www.jb.man.ac.uk/~bretonr/doublepulsar/movies/doublepulsar_eclipse.mov to learn more."  
+    
+        if failed == 0:
+            print("\n")        
+            print("\nThat's right! The pulsar's light is dipping because of a companion passing in front of it.")
+            print("\nAs you can see in this animation ... wait for it ...")
+            ## display animation1
+            tools_dir = '/content/drive/Shareddrives/ARIWS_timedomain/scripts'
+            movfile1 = os.path.join(tools_dir,'pulsar1.mov')
+            mov1 = open(movfile1,'rb').read()
+            data_url = "data:video/mp4;base64," + b64encode(mov1).decode()
+            display(HTML("""
+            <video width=600 controls>
+            <source src="%s">
+            </video>
+            """ % data_url))            
+            print("\nAnd the companion is another pulsar!")
+            print("\n")
+            print("\n")
+            print("\nBut to learn where these Zebra stripes come from you have to look at the details of the pulsar's magnetosphere.")
+            print("\nThe magnetosphere is the plasma trapped in the closed magnetic field lines of the pulsar, and can be modelled as a doughnut-shape.")
+            print("\nThis animation shows you what is going on ...")
+            ## display animation2
+            movfile = os.path.join(tools_dir,'pulsar.mov')
+            #print(movfile)
+            mov = open(movfile,'rb').read()
+            data_url = "data:video/mp4;base64," + b64encode(mov).decode()
+            display(HTML("""
+            <video width=600 controls>
+            <source src="%s">
+            </video>
+            """ % data_url))
+            print("The movie can also be found at http://www.jb.man.ac.uk/~bretonr/doublepulsar/movies/doublepulsar_eclipse.mov")
             break              
-   
-        print
-        guess = raw_input("guess a character:") 
+        guess = input("\n\nguess a character: ") 
         guesses += guess                    
-   
+    
         if guess not in word:  
          # turns counter decreases
             turns -= 1        
-            print "Wrong"    
-        # how many turns are left
-            print "You have", + turns, 'more guesses' 
+            print ("\nWrong")    
+            # how many turns are left
+            print("\nYou have", + turns, 'more guesses')
             if turns == 0:           
-                print "Oops! You lose! But you can try again!"
-   
-
+                print("\nOops! You lose! But you can try again!")
